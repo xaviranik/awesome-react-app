d9daef544f21c984eda09d80aad6016fc31d1514
xavir <xaviranik.me@gmail.com>
2021-01-30 14:07:47 +0600

    add: nested components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const Greeting = () => {
+const Booklist = () => {
   return (
     <>
-      <div className="test">Hello World</div>
-      <h1>This is awesome!</h1>
+      <section>This is a booklist</section>
+      <Book />
     </>
   );
 };
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg"
+      alt="img"
+    />
+  );
+};
+
+const Title = () => {
+  return <h2>A Promised Land</h2>;
+};
+
+const Author = () => {
+  return <h3>Barrack Obama</h3>;
+};
+
+ReactDom.render(<Booklist />, document.getElementById("root"));
