c870e3e99a3398207902f156c0c8d0baf0ccb6f1
xavir <xaviranik.me@gmail.com>
2021-01-31 14:10:54 +0600

    add: javascript in JSX

--- src/index.js
+++ src/index.js
@@ -7,39 +7,26 @@ import "./index.css";
 const Booklist = () => {
   return (
     <>
-      <section>This is a booklist</section>
       <Book />
     </>
   );
 };
 
+const author = "Barrack Obama";
+
 const Book = () => {
+  const title = "A Promised Land";
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg"
+        alt="img"
+      />
+      <h1>{title.toUpperCase()}</h1>
+      <h3>{author}</h3>
     </article>
   );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg"
-      alt="img"
-    />
-  );
-};
-
-const Title = () => {
-  return (
-    <h2 style={{ color: "#617d98", fontSize: "2rem" }}>A Promised Land</h2>
-  );
-};
-
-const Author = () => {
-  return <h3>Barrack Obama</h3>;
 };
 
 ReactDom.render(<Booklist />, document.getElementById("root"));
