cc4f07b81a0b804da916bd9f3e987b4c75168ef1
xavir <xaviranik.me@gmail.com>
2021-02-20 22:43:28 +0600

    add: simple listing using map

--- src/index.js
+++ src/index.js
@@ -4,30 +4,40 @@ import ReactDom from "react-dom";
 // Styling
 import "./index.css";
 
-const book = {
-  author: "Barrack Obama",
-  title: "A Promised Land",
-  img: "https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg",
-};
+// Data
+const books = [
+  {
+    id: 1,
+    author: "Barrack Obama",
+    title: "A Promised Land",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg",
+  },
+  {
+    id: 2,
+    author: "Simon Sinek",
+    title: "Together Is Better",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51ye21lNSaL._SY366_BO1,204,203,200_.jpg",
+  },
+];
 
 const Booklist = () => {
   return (
     <>
-      <Book img={book.img} title={book.title} author={book.author} />
+      {books.map((book) => {
+        return <Book key={book.id} book={book} />;
+      })}
     </>
   );
 };
 
 const Book = (props) => {
-
   return (
     <article className="book">
-      <img
-        src = {props.img}
-        alt="img"
-      />
-      <h1>{props.title.toUpperCase()}</h1>
-      <h3>{props.author}</h3>
+      <img src={props.book.img} alt="img" />
+      <h1>{props.book.title.toUpperCase()}</h1>
+      <h3>{props.book.author}</h3>
     </article>
   );
 };
